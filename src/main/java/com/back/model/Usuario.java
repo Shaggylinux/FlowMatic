bd5a1764702b9b73c7f164f147d36c9f0344b408
codejuliza <juliethpinilla2007@outlook.com>
2026-03-23 12:45:58 -0500

    feat(model): SE

---
+++ src/main/java/com/back/model/Usuario.java
@@ -0,0 +1,31 @@
+package com.back.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Column(nullable = false)
+    private String nombre;
+
+    @NotBlank(message = "El correo es obligatorio")
+    @Email(message = "Ingresa un correo válido")
+    @Column(nullable = false, unique = true)
+    private String correo;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, message = "La contraseña debe tener mínimo 8 caracteres")
+    @Column(nullable = false)
+    private String contrasena;
+
+}
---
+++ src/main/java/com/back/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.repository;
+
+import com.back.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByCorreo(String correo);
+}
