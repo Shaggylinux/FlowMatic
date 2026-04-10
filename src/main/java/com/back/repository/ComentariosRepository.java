980df47cf124ddd9b51bc37ca7d7d48d2ed9176c
Shaggy <malacruz132@gmail.com>
2026-04-10 07:24:46 -0500

    Remove : Comentarios

--- src/main/java/com/back/repository/ComentariosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.back.repository;
-
-import com.back.model.Comentarios;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComentariosRepository extends JpaRepository<Comentarios, Long> {
-    Comentarios findByNombre(String nombre);
-}
